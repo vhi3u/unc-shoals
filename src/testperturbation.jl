362e8fe4dbe8902830491f03696fd60ac44103d7
vhi3u <victorinh0@outlook.com>
2025-12-08 11:13:41 -0500

    added sponges and forcings

--- src/testperturbation.jl
+++ src/testperturbation.jl
@@ -23,47 +23,110 @@ x_km, y_km, h = dshoal(Lx / 1e3, Ly / 1e3, σ, Hs, Nx)
 
 ib_grid = ImmersedBoundaryGrid(grid, GridFittedBottom(h))
 
-T₂ = 12.421hours
-V₂ = 0.1
-@inline V(x, y, z, t, p) = p.V₂ * sin(2π * t / p.T₂)
-@inline V(x, z, t, p) = V(x, zero(x), z, t, p)
+# boundary condition setup
 
-# @inline U(x, y, z, t, p) = p.U₂
-# @inline U(y, z, t, p) = U(zero(y), y, z, t, p)
+V₂ = 0.1
+@inline V(x, z, t, p) = p.V₂   # p always holds V₂
 
 H = Lz
 
-open_bc = OpenBoundaryCondition(V; parameters=(; V₂, T₂),
+open_bc = OpenBoundaryCondition(V; parameters=(; V₂),
     scheme=PerturbationAdvection())
 
 v_bcs = FieldBoundaryConditions(south=open_bc, north=open_bc)
 
-@inline ambient_temperature(y, z, t, H) = 12 + 4z / H
-@inline ambient_temperature(x, y, z, t, H) = ambient_temperature(y, z, t, H)
-ambient_temperature_bc = ValueBoundaryCondition(ambient_temperature; parameters=H)
-T_bcs = FieldBoundaryConditions(south=ambient_temperature_bc, north=ambient_temperature_bc)
 
-ambient_salinity_bc = ValueBoundaryCondition(32)
-S_bcs = FieldBoundaryConditions(south=ambient_salinity_bc, north=ambient_salinity_bc)
+# temperature and salinity boundary conditions
 
-# u_bcs = FieldBoundaryConditions(west = open_bc, east = open_bc)
+Tₙ(x, z, t) = 20.5
+Tₛ(x, z, t) = 24.5
+Sₙ(x, z, t) = 32.6
+Sₛ(x, z, t) = 35.5
 
-model = NonhydrostaticModel(; grid=ib_grid,
+Ls = 10e3
+τ = 1hours
+params = (; Lx=Lx, Ly=Ly, Ls=Ls, τ=τ, V₂=V₂)
+
+S_bcs = FieldBoundaryConditions(south=ValueBoundaryCondition(Sₛ), north=ValueBoundaryCondition(Sₙ))
+T_bcs = FieldBoundaryConditions(south=ValueBoundaryCondition(Tₛ), north=ValueBoundaryCondition(Tₙ))
+
+
+# mask function
+
+@inline function south_mask(x, y, z, p)
+    y0 = 0
+    y1 = p.Ls
+    if y0 <= y <= y1
+        yy = (y - y0) / (y1 - y0)
+        return yy
+    else
+        return 0.0
+    end
+end
+
+@inline function north_mask(x, y, z, p)
+    y0 = p.Ly - p.Ls
+    y1 = p.Ly
+    if y0 <= y <= y1
+        yy = (y - y0) / (y1 - y0)
+        return yy
+    else
+        return 0.0
+    end
+end
+
+# sponge functions
+@inline sponge_u(x, y, z, t, u, p) = -(south_mask(x, y, z, p) * u / p.τ + north_mask(x, y, z, p) * u / p.τ)
+@inline sponge_v(x, y, z, t, v, p) = -(south_mask(x, y, z, p) * (v - V(x, z, t, p)) / p.τ + north_mask(x, y, z, p) * (v - V(x, z, t, p)) / p.τ)
+@inline sponge_w(x, y, z, t, w, p) = -(south_mask(x, y, z, p) * w / p.τ + north_mask(x, y, z, p) * w / p.τ)
+
+@inline sponge_T(x, y, z, t, T, p) = -(south_mask(x, y, z, p) * (T - Tₛ(x, z, t)) / p.τ + north_mask(x, y, z, p) * (T - Tₙ(x, z, t)) / p.τ)
+@inline sponge_S(x, y, z, t, S, p) = -(south_mask(x, y, z, p) * (S - Sₛ(x, z, t)) / p.τ + north_mask(x, y, z, p) * (S - Sₙ(x, z, t)) / p.τ)
+
+# forcings
+FT = Forcing(sponge_T, field_dependencies=:T, parameters=params)
+FS = Forcing(sponge_S, field_dependencies=:S, parameters=params)
+Fᵤ = Forcing(sponge_u, field_dependencies=:u, parameters=params)
+Fᵥ = Forcing(sponge_v, field_dependencies=:v, parameters=params)
+F_w = Forcing(sponge_w, field_dependencies=:w, parameters=params)
+forcings = (u=Fᵤ, v=Fᵥ, w=F_w, T=FT, S=FS)
+
+# @inline ambient_temperature(y, z, t, H) = 12 + 4z / H
+# @inline ambient_temperature(x, y, z, t, H) = ambient_temperature(y, z, t, H)
+# ambient_temperature_bc = ValueBoundaryCondition(ambient_temperature; parameters=H)
+# T_bcs = FieldBoundaryConditions(south=ambient_temperature_bc, north=ambient_temperature_bc)
+
+# ambient_salinity_bc = ValueBoundaryCondition(32)
+# S_bcs = FieldBoundaryConditions(south=ambient_salinity_bc, north=ambient_salinity_bc)
+
+model = NonhydrostaticModel(
+    grid=ib_grid,
     tracers=(:T, :S),
     buoyancy=SeawaterBuoyancy(),
     closure=AnisotropicMinimumDissipation(),
     advection=WENO(order=5),
-    coriolis=FPlane(latitude=0),
-    boundary_conditions=(; T=T_bcs, S=S_bcs, v=v_bcs))
+    coriolis=FPlane(latitude=35.2480),
+    boundary_conditions=(; T=T_bcs, S=S_bcs, v=v_bcs),
+    #forcing=forcings
+)
 
-Tᵢ(x, y, z) = ambient_temperature(x, y, z, 0, H)
 
-set!(model, T=Tᵢ, S=32, v=V(0, 0, 0, 0, (; V₂, T₂)))
+Tᵢ(x, y, z) = Tₛ(x, y, z)
+Sᵢ(x, y, z) = Sₛ(x, y, z)
 
+uᵢ = 0.005 * rand(size(u)...)
+vᵢ = 0.005 * rand(size(v)...)
+wᵢ = 0.005 * rand(size(w)...)
+uᵢ .-= mean(uᵢ)
+vᵢ .-= mean(vᵢ)
+wᵢ .-= mean(wᵢ)
+vᵢ .+= 0.10
+
+set!(model, T=Tᵢ, S=Sᵢ, v=vᵢ)
 cfl_values = Float64[]
 cfl_times = Float64[]
 
-simulation = Simulation(model, Δt=5seconds, stop_time=100days)
+simulation = Simulation(model, Δt=5seconds, stop_time=20days)
 conjure_time_step_wizard!(simulation, cfl=0.7)
 
 start_time = time_ns() * 1e-9
@@ -77,6 +140,8 @@ overwrite_existing = true
 
 u, v, w = model.velocities
 @show model.velocities.v.boundary_conditions
+@show minimum(v)
+@show maximum(v)
 using Oceanostics: ErtelPotentialVorticity
 
 u_c = @at (Center, Center, Center) u
--- src/testvn.jl
+++ src/testvn.jl
@@ -13,16 +13,9 @@ using DataFrames
 
 @info "building domain"
 
-# parameters for boundary condition timescales (use later)
-τ_v = 6hours
-τ_ts = 6hours
-Lₛ = 10e3
-
 Lx, Ly, Lz = 100e3, 200e3, 50
 Nx, Ny, Nz = 30, 30, 10
 x, y, z = (0, Lx), (0, Ly), (-Lz, 0)
-
-params = (; Lx=Lx, Ly=Ly, Ls=Lₛ, τ_v=τ_v, τ_ts=τ_ts)
 
 grid = RectilinearGrid(CPU(); size=(Nx, Ny, Nz), halo=(4, 4, 4),
     x, y, z, topology=(Bounded, Bounded, Bounded))
@@ -35,18 +28,7 @@ x_km, y_km, h = dshoal(Lx / 1e3, Ly / 1e3, σ, Hs, Nx)
 
 ib_grid = ImmersedBoundaryGrid(grid, GridFittedBottom(h))
 
-V₂ = 0.1 # m/s
-T₂ = 12.421hours
-
-@inline V(x, y, z, t, p) = p.V₂ * sin(2π * t / p.T₂)
-@inline V(x, z, t, p) = V(x, zero(x), z, t, p)
-
 H = Lz
-
-open_bc = OpenBoundaryCondition(V; scheme=PerturbationAdvection(inflow_timescale=τ_v, outflow_timescale=τ_v),
-    parameters=(; V₂, T₂))
-
-v_bcs = FieldBoundaryConditions(south=open_bc, north=open_bc)
 
 @info "loading B1 and B2 T/S profiles"
 using CSV, Interpolations
@@ -73,8 +55,7 @@ iS_north = extrapolate(interpolate((z_data,), S_north, Gridded(Linear())), Inter
 @inline snbc(x, y, z, t) = snbc(x, z, t)
 
 Tₑ(x, y, z) = 23.11
-Sₑ(x, y, z) = 36.4
-
+Sₑ(x, y, z) = 35.5
 
 @info "setting up boundary conditions"
 
@@ -86,6 +67,122 @@ salinN = ValueBoundaryCondition(snbc)
 T_bcs = FieldBoundaryConditions(south=tempS, north=tempN) #, east=tempE)
 S_bcs = FieldBoundaryConditions(south=salinS, north=salinN) #, east=salinE)
 
+# forcings
+
+# north and south sponge
+
+Lₛ = 10e3
+Lₑ = 10e3
+τₛ = 1days
+τₙ = 1days
+τₑ = 1days
+τ_ts = 1days
+
+params = (; Lx=Lx, Ly=Ly, Ls=Lₛ, Le=Lₑ, τₙ=τₙ, τₛ=τₛ, τₑ=τₑ, τ_ts=τ_ts)
+
+# velocity sigmoid
+@inline function v∞(x, z, t, p)
+    xC = 3e3
+    xS = 60e3
+    Lw = p.Lx
+    k1 = 80 / Lw
+    k2 = 40 / Lw
+
+    s1 = 1 / (1 + exp(-k1 * (x - xC)))
+    s2 = 1 / (1 + exp(k2 * (x - xS)))
+    s = (s1 - 1) + s2
+    sc = clamp(s, 0.0, 1.0)
+    return 0.10 * sc
+end
+
+# x sigmoid for north and south masks
+@inline function x_sigmoid(x, p)
+    xS = 60e3
+    Lw = p.Lx
+    k = 40 / Lw
+    s = 1 / (1 + exp(k * (x - xS)))
+    return 0.1 + 0.9 * s
+end
+
+@inline function south_mask(x, y, z, p)
+    y0 = 0
+    y1 = p.Ls
+    if y0 <= y <= y1
+        yy = (y - y0) / (y1 - y0)
+        return x_sigmoid(x, p) * yy
+    else
+        return 0.0
+    end
+end
+
+@inline function north_mask(x, y, z, p)
+    y0 = p.Ly - p.Ls
+    y1 = p.Ly
+
+    if y0 <= y <= y1
+        yy = (y - y0) / (y1 - y0)
+        return x_sigmoid(x, p) * yy
+    else
+        return 0.0
+    end
+end
+
+@inline function east_mask(x, y, z, p)
+    x0 = p.Lx - p.Le
+    x1 = p.Lx
+
+    if x0 <= x <= x1
+        return (x - x0) / (x1 - x0)
+    else
+        return 0.0
+    end
+end
+
+# eastern mask included, v sponge included
+
+@inline sponge_u(x, y, z, t, u, p) = -min(
+    south_mask(x, y, z, p) * u / p.τₛ,
+    north_mask(x, y, z, p) * u / p.τₙ,
+    east_mask(x, y, z, p) * u / p.τₑ)
+
+@inline sponge_v(x, y, z, t, v, p) = -min(
+    south_mask(x, y, z, p) * (v - v∞(x, z, t, p)) / p.τₛ,
+    north_mask(x, y, z, p) * (v - v∞(x, z, t, p)) / p.τₙ,
+    east_mask(x, y, z, p) * (v - v∞(x, z, t, p)) / p.τₑ)
+
+@inline sponge_w(x, y, z, t, w, p) = -min(
+    south_mask(x, y, z, p) * w / p.τₙ,
+    north_mask(x, y, z, p) * w / p.τₛ,
+    east_mask(x, y, z, p) * w / p.τₑ)
+
+@inline sponge_T(x, y, z, t, T, p) = -min(
+    south_mask(x, y, z, p) * (T - tsbc(x, y, z, t)) / p.τ_ts,
+    north_mask(x, y, z, p) * (T - tnbc(x, y, z, t)) / p.τ_ts,
+    east_mask(x, y, z, p) * (T - Tₑ(x, y, z)) / p.τₑ)
+
+@inline sponge_S(x, y, z, t, S, p) = -min(
+    south_mask(x, y, z, p) * (S - ssbc(x, y, z, t)) / p.τ_ts,
+    north_mask(x, y, z, p) * (S - snbc(x, y, z, p)) / p.τ_ts,
+    east_mask(x, y, z, p) * (S - Sₑ(x, y, z)) / p.τₑ)
+
+# build forcing function
+
+FT = Forcing(sponge_T, field_dependencies=:T, parameters=params)
+FS = Forcing(sponge_S, field_dependencies=:S, parameters=params)
+Fᵤ = Forcing(sponge_u, field_dependencies=:u, parameters=params)
+Fᵥ = Forcing(sponge_v, field_dependencies=:v, parameters=params)
+F_w = Forcing(sponge_w, field_dependencies=:w, parameters=params)
+forcings = (u=Fᵤ, v=Fᵥ, w=F_w, T=FT, S=FS)
+# boundary conditions
+
+open_bc = OpenBoundaryCondition(v∞; scheme=PerturbationAdvection(), parameters=params)
+
+v_bcs = FieldBoundaryConditions(north=open_bc, south=open_bc)
+u_bcs = FieldBoundaryConditions()
+w_bcs = FieldBoundaryConditions()
+
+bcs = (u=u_bcs, v=v_bcs, w=w_bcs, T=T_bcs, S=S_bcs)
+
 
 model = NonhydrostaticModel(;
     grid=ib_grid,
@@ -96,8 +193,8 @@ model = NonhydrostaticModel(;
     closure=AnisotropicMinimumDissipation(),
     advection=WENO(order=5),
     coriolis=FPlane(latitude=35.2480),
-    boundary_conditions=(; T=T_bcs, v=v_bcs, S=S_bcs)
-)
+    boundary_conditions=bcs,
+    forcing=forcings)
 
 
 # make sure T/S profiles are doing their job...
@@ -106,7 +203,7 @@ zC = znodes(ib_grid, Center())
 cfl_values = Float64[]       # Stores CFL at each step
 cfl_times = Float64[]       # Stores model time
 
-simulation = Simulation(model, Δt=5seconds, stop_time=100days)
+simulation = Simulation(model, Δt=5seconds, stop_time=20days)
 conjure_time_step_wizard!(simulation, cfl=0.7)
 
 start_time = time_ns() * 1e-9
@@ -185,16 +282,34 @@ plot!(p2, S_south_model, zc;
 plt = plot(p1, p2; layout=(1, 2), size=(600, 300))
 display(plt)
 
-set!(model, T=Tᵢ, S=Sᵢ, v=V(0, 0, 0, 0, (; V₂, T₂)))
+# initial conditions
+uᵢ = 0.005 * rand(size(u)...)
+vᵢ = 0.005 * rand(size(v)...)
+wᵢ = 0.005 * rand(size(w)...)
+
+uᵢ .-= mean(uᵢ)
+vᵢ .-= mean(vᵢ)
+wᵢ .-= mean(wᵢ)
+xv, yv, zv = nodes(v, reshape=true)
+vᵢ .+= v∞.(xv, zv, 0.0, Ref(params))
+
+@inline α_lin(y) = clamp(y / Ly, 0.0, 1.0)
+@inline blend(a, b, α) = (1 - α) * a + α * b
+@inline Tᵢ(x, y, z) = blend(iT_south(z), iT_north(z), α_lin(y))
+@inline Sᵢ(x, y, z) = blend(iS_south(z), iS_north(z), α_lin(y))
+
+set!(model, u=uᵢ, v=vᵢ, w=wᵢ, T=Tᵢ, S=Sᵢ)
+
+# set!(model, T=Tᵢ, S=Sᵢ, v=V(0, 0, 0, 0, (; V₂, T₂)))
 
 println(model)              # prints a structured summary
 
 run!(simulation)
 
-using Oceananigans.Models.NonhydrostaticModels: north_mass_flux, south_mass_flux
+# using Oceananigans.Models.NonhydrostaticModels: north_mass_flux, south_mass_flux
 
-north_flux = north_mass_flux(v)
-south_flux = south_mass_flux(v)
-println("North flux: ", north_flux)
-println("South flux: ", south_flux)
+# north_flux = north_mass_flux(v)
+# south_flux = south_mass_flux(v)
+# println("North flux: ", north_flux)
+# println("South flux: ", south_flux)
 
