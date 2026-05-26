31f4c9e003585910d60deecde74f6f1a86ad8c19
vhi3u <victorinh0@outlook.com>
2026-05-26 15:21:43 -0400

    TEST 11: horizontal and vertical scalar diffusivity

--- src/flow_over_shoals_sweep.jl
+++ src/flow_over_shoals_sweep.jl
@@ -433,7 +433,7 @@ if periodic_y
     model = NonhydrostaticModel(ib_grid;
         timestepper=:RungeKutta3,
         advection=WENO(order=5),
-        closure=VerticalScalarDiffusivity(ν=1e-2, κ=1e-2),
+        closure=(HorizontalScalarDiffusivity(ν=1e-5, κ=1e-5), VerticalScalarDiffusivity(ν=1e-2, κ=1e-2)),
         hydrostatic_pressure_anomaly=CenterField(ib_grid),
         pressure_solver=ConjugateGradientPoissonSolver(ib_grid, reltol=reltol, abstol=abstol, maxiter=100),
         tracers=(:T, :S),
@@ -446,7 +446,7 @@ else
     model = NonhydrostaticModel(ib_grid;
         timestepper=:RungeKutta3,
         advection=WENO(order=5),
-        closure=VerticalScalarDiffusivity(ν=1e-2, κ=1e-2),
+        closure=(HorizontalScalarDiffusivity(ν=1e-5, κ=1e-5), VerticalScalarDiffusivity(ν=1e-2, κ=1e-2)),
         hydrostatic_pressure_anomaly=CenterField(ib_grid),
         pressure_solver=ConjugateGradientPoissonSolver(ib_grid, reltol=reltol, abstol=abstol, maxiter=100),
         tracers=(:T, :S),
