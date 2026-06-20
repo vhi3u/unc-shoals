a073eeba210cb88be69934f8790e6c54fdc0c779
vhi3u <victorinh0@outlook.com>
2026-06-20 18:52:47 +0700

    GPU testing

--- src/flow_over_shoals.jl
+++ src/flow_over_shoals.jl
@@ -52,7 +52,7 @@ include(joinpath(@__DIR__, "dshoal_vn_param.jl"))
 # simulation knobs
 # ═══════════════════════════════════════════════════════════════════════════
 run_number = 5
-sim_runtime = 20days
+sim_runtime = 100days
 callback_interval = 86400seconds
 run_tag = (periodic_y ? "periodic" : "bounded") * "_shoals$(run_number)"
 
@@ -110,7 +110,7 @@ params = (; params...,
     Ls=40e3,
     Le=100e3,
     Lw=10e3,
-    τ=5days,
+    τ=1days,
     T_north_v1=T_north_v1,
     T_south_v1=T_south_v1,
     S_north_v1=S_north_v1,
