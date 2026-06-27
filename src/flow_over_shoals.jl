f9d4c82a3fba72218d005c38eb4439db36947b34
vhi3u <victorinh0@outlook.com>
2026-06-27 20:49:53 +0700

    Horizontal(10, 10) Vertical(1e-4, 1e-4)

--- src/flow_over_shoals.jl
+++ src/flow_over_shoals.jl
@@ -372,7 +372,7 @@ if periodic_y
     model = NonhydrostaticModel(ib_grid;
         timestepper=:RungeKutta3,
         advection=WENO(order=5),
-        closure=AnisotropicMinimumDissipation(),
+        closure=(HorizontalScalarDiffusivity(ν=10.0, κ=10.0), VerticalScalarDiffusivity(ν=1e-4, κ=1e-4)),
         hydrostatic_pressure_anomaly=CenterField(ib_grid),
         pressure_solver=ConjugateGradientPoissonSolver(ib_grid, reltol=reltol, abstol=abstol, maxiter=100),
         tracers=(:T, :S),
