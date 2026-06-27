84aa2c81937c8947c2df0382a6b8db58d1f601fa
vhi3u <victorinh0@outlook.com>
2026-06-28 01:09:21 +0700

    closure=(HorizontalScalarDiffusivity(ν=5, κ=5), VerticalScalarDiffusivity(ν=1e-4, κ=1e-4))

--- src/flow_over_shoals.jl
+++ src/flow_over_shoals.jl
@@ -51,7 +51,7 @@ include(joinpath(@__DIR__, "dshoal_vn_param.jl"))
 # ═══════════════════════════════════════════════════════════════════════════
 # simulation knobs
 # ═══════════════════════════════════════════════════════════════════════════
-run_number = 21
+run_number = 22
 sim_runtime = 30days
 callback_interval = 86400seconds
 run_tag = (periodic_y ? "periodic" : "bounded") * "_shoals$(run_number)"
@@ -372,7 +372,7 @@ if periodic_y
     model = NonhydrostaticModel(ib_grid;
         timestepper=:RungeKutta3,
         advection=WENO(order=5),
-        closure=(HorizontalScalarDiffusivity(ν=0.0015, κ=0.0015), VerticalScalarDiffusivity(ν=1e-3, κ=1e-3)),
+        closure=(HorizontalScalarDiffusivity(ν=5, κ=5), VerticalScalarDiffusivity(ν=1e-4, κ=1e-4)),
         #closure=AnisotropicMinimumDissipation(),
         hydrostatic_pressure_anomaly=CenterField(ib_grid),
         pressure_solver=ConjugateGradientPoissonSolver(ib_grid, reltol=reltol, abstol=abstol, maxiter=100),
