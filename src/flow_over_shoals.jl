eabe324d1d94a730526a4109e637b972c2020390
vhi3u <victorinh0@outlook.com>
2026-06-28 13:58:22 +0700

    closure=(HorizontalScalarDiffusivity(ν=1e-4, κ=1e-4), VerticalScalarDiffusivity(ν=1e-6, κ=1e-6))

--- src/flow_over_shoals.jl
+++ src/flow_over_shoals.jl
@@ -51,7 +51,7 @@ include(joinpath(@__DIR__, "dshoal_vn_param.jl"))
 # ═══════════════════════════════════════════════════════════════════════════
 # simulation knobs
 # ═══════════════════════════════════════════════════════════════════════════
-run_number = 23
+run_number = 24
 sim_runtime = 50days
 callback_interval = 86400seconds
 run_tag = (periodic_y ? "periodic" : "bounded") * "_shoals$(run_number)"
@@ -372,8 +372,8 @@ if periodic_y
     model = NonhydrostaticModel(ib_grid;
         timestepper=:RungeKutta3,
         advection=WENO(order=5),
-        # closure=(HorizontalScalarDiffusivity(ν=5, κ=5), VerticalScalarDiffusivity(ν=1e-4, κ=1e-4)),
-        closure=TKEDissipationVerticalDiffusivity(),
+        closure=(HorizontalScalarDiffusivity(ν=1e-4, κ=1e-4), VerticalScalarDiffusivity(ν=1e-6, κ=1e-6)),
+        # closure=TKEDissipationVerticalDiffusivity(ExplicitTimeDiscretization()),
         hydrostatic_pressure_anomaly=CenterField(ib_grid),
         pressure_solver=ConjugateGradientPoissonSolver(ib_grid, reltol=reltol, abstol=abstol, maxiter=100),
         tracers=(:T, :S),
